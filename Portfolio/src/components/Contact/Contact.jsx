a5c9c6099c0442ee4a8cbec7ca94272037bea4f8
A-Kederys <your-email@example.com>
2024-10-21 19:08:59 +0300

    Added dark/light mode, fixed some styles

--- Portfolio/src/components/Contact/Contact.jsx
+++ Portfolio/src/components/Contact/Contact.jsx
@@ -121,6 +121,8 @@ function Contact() {
             </div>
         </div>
     </footer>
+
+    
   )
 }
 
--- Portfolio/src/components/Navbar/Navbar.jsx
+++ Portfolio/src/components/Navbar/Navbar.jsx
@@ -5,6 +5,17 @@ import {getImageURL} from "../../imgPath";
 function Navbar() {
 
     const [isMenuOpen, setMenuOpen] = useState(false);
+
+    const switchTheme = (e) => {
+
+        if(e.target.checked) {
+            document.querySelector('body').setAttribute('data-theme', 'light')
+        }
+
+        else {
+            document.querySelector('body').setAttribute('data-theme', 'dark')
+        }
+    }
     
   return (
     <div className={styles.navWrap}>
@@ -24,9 +35,23 @@ function Navbar() {
                 <ul className={`${styles.menuItems} ${isMenuOpen && styles.menuOpen}`}
                     onClick= {() => setMenuOpen(false)} 
                 >
+                    <label>
+                        <input className='toggle-checkbox' type='checkbox' onChange={switchTheme}></input>
+                        <div className='toggle-slot'>
+                            <div className='sun-icon-wrapper'>
+                            <div className="iconify sun-icon" data-icon="feather-sun" data-inline="false"></div>
+                            </div>
+                            <div className='toggle-button'></div>
+                            <div className='moon-icon-wrapper'>
+                            <div className="iconify moon-icon" data-icon="feather-moon" data-inline="false"></div>
+                            </div>
+                        </div>
+                    </label>
+
                     <li><a href="#About">About</a></li>
                     <li><a href="#Projects">Projects</a></li>
                     <li><a href="#Contact">Contact</a></li>
+
                 </ul>
             </div>
         </nav>
